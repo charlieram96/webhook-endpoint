f4624edd0a1685d9975c87a67087edde997da969
charlieram96 <charlieram96@gmail.com>
2023-07-31 03:10:52 -0400

    email

--- pages/api/order.js
+++ pages/api/order.js
@@ -3,24 +3,29 @@ import mail from '@sendgrid/mail';
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
-  try {
-    await mail.send({
-      from: 'charlieram96@gmail.com',
-      to: 'charlieramirez96@gmail.com',
-      subject: 'Order created',
-      html: `<div>${JSON.stringify(orderData)}</div>`,
-    });
+  // Check if the request method is POST
+  if (req.method === 'POST') {
+    // Parse the order data from the request body
+    const orderData = req.body;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent.' }),
-    };
-  }
-  catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Email failed.' }),
-    };
-  }
+    try {
+      // Try to send the email
+      await mail.send({
+        from: 'charlieram96@gmail.com',
+        to: 'charlieramirez96@gmail.com',
+        subject: 'Order created',
+        html: `<div>${JSON.stringify(orderData)}</div>`,
+      });
 
+      // Respond with a success status and message
+      res.status(200).json({ message: 'Email sent.' });
+    } catch (error) {
+      // Log the error and respond with an error status and message
+      console.error(error);
+      res.status(500).json({ message: `Email failed: ${error.message}` });
+    }
+  } else {
+    // If the request method isn't POST, respond with a 405 'Method Not Allowed' status
+    res.status(405).json({ error: 'Method not allowed' });
+  }
 }

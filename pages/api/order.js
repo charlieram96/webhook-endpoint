75749db748f58e6b72779d4b63b1dc461fbba7c0
charlieram96 <charlieram96@gmail.com>
2023-07-31 17:55:57 -0400

    email

--- pages/api/order.js
+++ pages/api/order.js
@@ -8,13 +8,28 @@ export default async function handler(req, res) {
     // Parse the order data from the request body
     const orderData = req.body;
 
+    const note = orderData.note;
+    let formattedNote = note.replace(/\\n/g, '').replace(/\\"/g, '"');
+    console.log(formattedNote); // This will output your JSON as a string without newline characters and with unescaped quotes.
+
+    let firstName = formattedNote.first_name;
+    let lastName = formattedNote.last_name;
+    let department = formattedNote.department;
+    let tickets = formattedNote.tickets;
+
     try {
       // Try to send the email
       await mail.send({
         from: 'charlieram96@gmail.com',
         to: 'charlieramirez96@gmail.com',
         subject: 'Order created',
-        html: `<div>${JSON.stringify(orderData)}</div>`,
+        html: `
+          <div>
+            First name: ${firstName}
+            Last name: ${lastName}
+            Department: ${department}
+            Tickets: ${tickets}
+          </div>`,
       });
 
       // Respond with a success status and message

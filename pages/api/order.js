ca2555b5b64c2edb52fcd87d45cd293442cd555d
charlieram96 <charlieram96@gmail.com>
2023-09-02 02:02:07 -0400

    updated image

--- pages/api/order.js
+++ pages/api/order.js
@@ -47,16 +47,16 @@ export default async function handler(req, res) {
             to: recipient.email,
             subject: 'Gift In Your Name',
             html: `
-            <img src="https://maw.cerberus.com/images/curves-low-opacity.png" alt="curves" style="display: block; position: absolute; bottom: 0; left: 0;" width="709" height="279" />
-            <div style="position: relative; padding: 15px 50px 50px 50px; border-radius: 0px; background-color: ##D9E5FE; color: #657aa5 !important; border: 3px solid #c5d6f8; font-weight: 800;">
-            <div style="position: relative; z-index: 1;">
-              <br><br>
-              Dear ${recipient.firstName} ${recipient.lastName}, <br><br>
-              ${donorFirstName} ${donorLastName} has made a generous gift in your name for our Annual Make-A-Wish Fundraiser! This contribution will go towards supporting the Make-A-Wish Foundation in their mission to grant the wishes of children with critical illnesses.
-              <br><br>Warm regards,
-              <br><br>
-              Cerberus DEI Team
+            <div style="position: relative; border-radius: 0px; background-color: #D9E5FE; color: #657aa5 !important; border: 3px solid #c5d6f8; font-weight: 800;">
+            <div style="position: relative; z-index: 1; padding: 15px 50px 50px 50px;">
+            <br><br>
+            Dear ${recipient.firstName} ${recipient.lastName}, <br><br>
+            ${donorFirstName} ${donorLastName} has made a generous gift in your name for our Annual Make-A-Wish Fundraiser! This contribution will go towards supporting the Make-A-Wish Foundation in their mission to grant the wishes of children with critical illnesses.
+            <br><br>Warm regards,
+            <br><br>
+            Cerberus DEI Team
             </div>
+            <img src="https://maw.cerberus.com/images/curves-low-opacity.png" alt="curves" style="display: block; position: absolute !important; width: 100% !important; bottom: 0 !important; left: 0 !important; object-fit: contain !important;" width="709" height="279" />
           </div>`,
           });
         }

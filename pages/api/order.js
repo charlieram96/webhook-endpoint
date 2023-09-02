03d26f14fc30cc928a6f43d57c0b93e0b5cd7c89
charlieram96 <charlieram96@gmail.com>
2023-09-02 02:10:54 -0400

    updated image

--- pages/api/order.js
+++ pages/api/order.js
@@ -48,16 +48,16 @@ export default async function handler(req, res) {
             subject: 'Gift In Your Name',
             html: `
             <div style="position: relative; border-radius: 0px; background-color: #D9E5FE; color: #657aa5 !important; border: 3px solid #c5d6f8; font-weight: 800;">
-            <div style="position: relative; z-index: 1; padding: 15px 50px 50px 50px;">
-            <br><br>
-            Dear ${recipient.firstName} ${recipient.lastName}, <br><br>
-            ${donorFirstName} ${donorLastName} has made a generous gift in your name for our Annual Make-A-Wish Fundraiser! This contribution will go towards supporting the Make-A-Wish Foundation in their mission to grant the wishes of children with critical illnesses.
-            <br><br>Warm regards,
-            <br><br>
-            Cerberus DEI Team
-            </div>
-            <img src="https://maw.cerberus.com/images/curves-low-opacity.png" alt="curves" style="display: block; position: absolute !important; width: 100% !important; bottom: 0 !important; left: 0 !important; object-fit: contain !important;" width="709" height="279" />
-          </div>`,
+              <div style="position: relative; z-index: 1; padding: 15px 50px 10px 50px;">
+                <br><br>
+                Dear ${recipient.firstName} ${recipient.lastName}, <br><br>
+                ${donorFirstName} ${donorLastName} has made a generous gift in your name for our Annual Make-A-Wish Fundraiser! This contribution will go towards supporting the Make-A-Wish Foundation in their mission to grant the wishes of children with critical illnesses.
+                <br><br>Warm regards,
+                <br><br>
+                Cerberus DEI Team
+                </div>
+              <img src="https://maw.cerberus.com/images/curves-low-opacity.png" alt="curves" style="display: block; position: absolute !important; width: 100% !important; bottom: 0 !important; left: 0 !important; object-fit: cover !important;" width="1692" height="540" />
+            </div>`,
           });
         }
       }));

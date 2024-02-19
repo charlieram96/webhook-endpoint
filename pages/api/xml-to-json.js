50408b859fd32614f612682cc3f0967bbe6bd8e5
charlieram96 <charlieram96@gmail.com>
2024-02-19 11:57:32 -0500

    new endpoint

--- pages/api/xml-to-json.js
+++ pages/api/xml-to-json.js
@@ -1,12 +1,11 @@
-// Import Axios
 import axios from 'axios';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Assuming `req.body` contains the form data as JSON
+    // Log the data being forwarded
+    console.log('Forwarding data:', JSON.stringify(req.body.formData));
 
     try {
-      // Forward the form data to the specified endpoint
       const response = await axios.post('https://dev-marko.aramark.net/v1/forms/capture', req.body.formData, {
         headers: {
           'apikey': '4AzMhe9mqSSzrTM0IR9ORSBqrGTcV6Al',
@@ -14,7 +13,6 @@ export default async function handler(req, res) {
         }
       });
 
-      // Handle response from the forwarded request
       console.log('Forwarded data response:', response.data);
       res.status(200).json({ message: 'Data forwarded successfully', forwardedResponse: response.data });
     } catch (error) {
@@ -22,7 +20,6 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Failed to forward data', details: error.response ? error.response.data : error.message });
     }
   } else {
-    // Handle any non-POST requests
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
